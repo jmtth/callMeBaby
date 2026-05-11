b778bc861641ee281b61b7952bca84e6399b0877
jhervoch <marvin@42fr>
2026-05-11 16:13:17 +0200

    feat: add tests for is_valid_number_fragment function

--- tests/test_utils_number.py
+++ tests/test_utils_number.py
@@ -1,5 +1,6 @@
 from src import utils
 
+
 def test_extract_decimal_counts():
     assert utils.extract_decimal_counts("123.45") == [2]
     assert utils.extract_decimal_counts("123.45 123.3") == [2, 1]
@@ -15,3 +16,22 @@ def test_extract_decimal_counts():
     assert utils.extract_decimal_counts("-.5e-5") == [1, 1]
     assert utils.extract_decimal_counts("fjhd.fj") == []
 
+def test_is_valid_number_fragment():
+    assert utils.is_valid_number_fragment("") is True
+    assert utils.is_valid_number_fragment("123") is True
+    assert utils.is_valid_number_fragment("-123") is True
+    assert utils.is_valid_number_fragment("1e1") is True
+    assert utils.is_valid_number_fragment("-1e-") is True
+    assert utils.is_valid_number_fragment("1.5e+10") is True
+    assert utils.is_valid_number_fragment("123.") is False
+    assert utils.is_valid_number_fragment("-123.") is False
+    assert utils.is_valid_number_fragment("1e") is False
+    assert utils.is_valid_number_fragment("1e-") is False
+    assert utils.is_valid_number_fragment("1e+") is False
+    assert utils.is_valid_number_fragment(".e10") is False
+    assert utils.is_valid_number_fragment("-.e10") is False
+    assert utils.is_valid_number_fragment("abc") is False
+    assert utils.is_valid_number_fragment("1.2.3") is False
+    assert utils.is_valid_number_fragment("1e2.3") is False
+    assert utils.is_valid_number_fragment("1e-2.3") is False
+    assert utils.is_valid_number_fragment("1e-2e3") is False
